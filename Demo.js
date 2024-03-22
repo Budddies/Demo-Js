5cc270c496015af2c278cd963590fe77da3cc299
Maishank Makwana <58628325+maishankmsk@users.noreply.github.com>
2024-03-22 21:32:35 +0530

    add parellax js

--- Demo.js
+++ Demo.js
@@ -1,3 +1,27 @@
+//Parallax div element's scrolling effect with jQuery using CSS transform
+jQuery(document).ready(function($) {
+  //parallax scroll
+  $(window).on("load scroll", function() {
+    var parallaxElement = $(".parallax_scroll"),
+      parallaxQuantity = parallaxElement.length;
+    window.requestAnimationFrame(function() {
+      for (var i = 0; i < parallaxQuantity; i++) {
+        var currentElement = parallaxElement.eq(i),
+          windowTop = $(window).scrollTop(),
+          elementTop = currentElement.offset().top,
+          elementHeight = currentElement.height(),
+          viewPortHeight = window.innerHeight * 0.5 - elementHeight * 0.5,
+          scrolled = windowTop - elementTop + viewPortHeight;
+        currentElement.css({
+          transform: "translate3d(0," + scrolled * -0.15 + "px, 0)"
+        });
+      }
+    });
+  });
+});
+
+
+
 // One section scroll add class 
 jQuery(window).scroll(function() {
 		var scrollDistance = jQuery(window).scrollTop();
